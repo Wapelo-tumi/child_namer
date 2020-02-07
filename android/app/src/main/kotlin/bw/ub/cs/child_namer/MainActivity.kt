f9cbcae930583952318f3c5ef2c5e77aabcffbf4
unknown <201602869>
2020-02-07 10:04:51 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/bw/ub/cs/child_namer/MainActivity.kt
@@ -0,0 +1,12 @@
+package bw.ub.cs.child_namer
+
+import android.support.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
